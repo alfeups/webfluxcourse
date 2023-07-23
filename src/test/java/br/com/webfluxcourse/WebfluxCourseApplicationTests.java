56cdbe9b28c01c5a6a17225f46c25df53361b9c9
alfeups <alfeups@hotmail.com>
2023-07-23 17:46:46 +0100

    creating save for user

--- src/test/java/br/com/webfluxcourse/WebfluxCourseApplicationTests.java
+++ src/test/java/br/com/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.webfluxcourse;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WebfluxCourseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.webfluxcourse;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WebfluxCourseApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
