a82a58e278cf1a3ac2a77349f1a8815e19a2f3a3
alfeups <alfeups@hotmail.com>
2023-07-23 18:35:03 +0100

    add validation error by fields

--- src/main/java/br/com/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -3,12 +3,15 @@ package br.com.webfluxcourse.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
+import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
@@ -27,6 +30,25 @@ public class ControllerExceptionHandler {
                         .path(request.getPath().toString())
                         .build()
                 ));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(
+            WebExchangeBindException ex,
+            ServerHttpRequest request
+    ){
+        ValidationError error = new ValidationError(
+                now(),
+                request.getPath().toString(),
+                BAD_REQUEST.value(),
+                "Validation Error...",
+                "Error on validation attributes");
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()){
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
     }
 
     private String verificaDuplicatedKeys(String message){
--- src/main/java/br/com/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/webfluxcourse/model/request/UserRequest.java
@@ -1,7 +1,22 @@
 package br.com.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest (
-    String name,
-    String email,
-    String password
+        //@TrimString
+        @NotBlank(message = "must not be null or empty")
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
+        String name,
+
+        //@TrimString
+        @Email(message = "invalid email")
+        @NotBlank(message = "must not be null or empty")
+        String email,
+
+        //@TrimString
+        @NotBlank(message = "must not be null or empty")
+        @Size(min = 3, max = 20, message = "must be between 3 and 20 characters")
+        String password
     ) { }
